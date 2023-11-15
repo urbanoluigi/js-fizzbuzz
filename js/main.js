f304ccdb4ee40095ab22bca9358e5bdbecd2edd0
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-15 16:11:53 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 //se il numero è un multiplo di 5 'buzz' invece del numero
 //se il numero è un multiplo di 3 e 5 'fizzbuzz' invece del numero
 
+//creo una funzione che accetti 2 paramentri, uno iniziale e uno finale
+function fizzbuzz (start, end) {
+//dobbiamo stampare ogni numero da 1 a 100
+for(let i=0; <100; i++){
+
+}
+}
